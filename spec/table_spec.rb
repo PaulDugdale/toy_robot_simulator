e941550f244f9b3f2aac3684ced5c66d231a659b
Paul Dugdale <paul.dugdale@velodec.com>
2025-05-08 17:19:40 +0100

    Fix formatting with standardrb

--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -1,23 +1,23 @@
-require 'table'
+require "table"
 
 RSpec.describe Table do
   subject(:table) { Table.new }
 
-  describe '#initialize' do
-    it 'has a dimension of 5x5' do
+  describe "#initialize" do
+    it "has a dimension of 5x5" do
       expect(table.width).to eq(5)
       expect(table.height).to eq(5)
     end
   end
 
-  describe '#position_valid?' do
-    it 'returns true when the position is valid' do
+  describe "#position_valid?" do
+    it "returns true when the position is valid" do
       expect(table.position_valid?(0, 0)).to be true
       expect(table.position_valid?(4, 4)).to be true
       expect(table.position_valid?(1, 3)).to be true
     end
 
-    it 'returns false when the position is out of bounds' do
+    it "returns false when the position is out of bounds" do
       expect(table.position_valid?(-1, 0)).to be false
       expect(table.position_valid?(0, -1)).to be false
       expect(table.position_valid?(5, 0)).to be false
