54894674c78bcf93223b45caa0060ed130e7db56
Paul Dugdale <paul.dugdale@velodec.com>
2025-05-09 12:09:17 +0100

    Add facing to robot

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,9 +1,21 @@
 class Robot
-  attr_reader :x, :y, :table
+  FACINGS = ["NORTH", "SOUTH", "EAST", "WEST"].freeze
+
+  attr_reader :x, :y, :facing, :table
 
   def initialize(table)
     @table = table
     @x = 0
     @y = 0
+  end
+
+  def place(x, y, facing)
+    return false unless table.position_valid?(x, y)
+    return false unless FACINGS.include?(facing)
+
+    @x = x
+    @y = y
+    @facing = facing
+    true
   end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -12,4 +12,32 @@ RSpec.describe Robot do
       expect(robot.y).to eq(0)
     end
   end
+
+  describe "#place" do
+    it "places the robot with a valid position and facing" do
+      expect(robot.place(1, 1, "NORTH")).to be true
+      expect(robot.place(2, 3, "SOUTH")).to be true
+      expect(robot.place(3, 4, "EAST")).to be true
+      expect(robot.place(0, 0, "WEST")).to be true
+    end
+
+    it "sets the position and facing when valid" do
+      expect(robot.place(1, 2, "NORTH")).to be true
+      expect(robot.x).to eq(1)
+      expect(robot.y).to eq(2)
+      expect(robot.facing).to eq("NORTH")
+    end
+
+    it "doesn't place the robot with a invalid position" do
+      expect(robot.place(-1, 0, "NORTH")).to be false
+      expect(robot.place(0, -1, "SOUTH")).to be false
+      expect(robot.place(5, 0, "EAST")).to be false
+      expect(robot.place(0, -5, "WEST")).to be false
+    end
+
+    it "doesn't place the robot with a invalid facing" do
+      expect(robot.place(0, 0, "UNKNOWN")).to be false
+      expect(robot.place(0, 0, "")).to be false
+    end
+  end
 end
